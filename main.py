410d90a6008105283f00afb8030acaa353e9acb4
imshibl <imshibil36@gmail.com>
2024-02-21 17:38:26 +0530

    update

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ st.set_page_config(page_title="Better Resume", page_icon=":memo:", layout="wide"
 # favicon being an object of the same kind as the one you should provide st.image() with (ie. a PIL array for example) or a string (url or local file path)
 
 
-st.title("Better Resume")
-st.header("Resume Builder")
-pdf_file = st.file_uploader("Select your Resume", type=["pdf"])
+def app():
+    st.title("Better Resume")
+    st.header("Resume Builder")
+    pdf_file = st.file_uploader("Select your Resume", type=["pdf"])
+
+
+if __name__ == "__main__":
+    app()
